eb874bf917531f49044e4a016ce32eb501d4ef87
guiyones <guiyonesnogara@gmail.com>
2025-01-06 11:03:49 -0300

    Created DB connection

--- main.go
+++ main.go
@@ -1,19 +1,54 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
-	ID    string
-	Name  string
-	Price float64
+	ID     string
+	Name   string
+	Price  float64
+	Weight int
 }
 
-func NewProduct(name string, price float64) *Product {
+func NewProduct(name string, price float64, weight int) *Product {
 	return &Product{
-		ID:    uuid.New().String(),
-		Name:  name,
-		Price: price,
+		ID:     uuid.New().String(),
+		Name:   name,
+		Price:  price,
+		Weight: weight,
 	}
 }
 
-func main() {}
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/brimos")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	product := NewProduct("Coalhada Sal", 6.20, 120)
+	err = InsertProduct(db, *product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func InsertProduct(db *sql.DB, product Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price, wieght) values(?,?,?,?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price, product.Weight)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
